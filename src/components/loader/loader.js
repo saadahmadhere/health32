961d655fb6dfe20b503e711ef766580c2531e35c
saadahmadhere <saadahmad728@gmail.com>
2025-02-22 15:51:17 +0530

    feat: add Loader

---
+++ src/components/loader/loader.js
@@ -0,0 +1,16 @@
+import './loader.css';
+
+const Loader = () => {
+	return (
+		<div class='sk-chase'>
+			<div class='sk-chase-dot'></div>
+			<div class='sk-chase-dot'></div>
+			<div class='sk-chase-dot'></div>
+			<div class='sk-chase-dot'></div>
+			<div class='sk-chase-dot'></div>
+			<div class='sk-chase-dot'></div>
+		</div>
+	);
+};
+
+export default Loader;
