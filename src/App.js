de623730e7a2d0cd2542d64af01f5494db46b5d8
saadahmadhere <saadahmad728@gmail.com>
2025-02-23 16:14:49 +0530

    update: App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Row, Col, Card } from 'antd';
-import {
-	DeleteFilled,
-	EditOutlined,
-	GlobalOutlined,
-	HeartFilled,
-	HeartOutlined,
-	MailOutlined,
-	PhoneOutlined,
-} from '@ant-design/icons';
+import { Row } from 'antd';
 
 import Loader from './components/loader/loader';
+import UserCard from './components/UserCard/UserCard';
 
 function App() {
 	const [users, setUsers] = useState([]);
@@ -24,7 +16,6 @@ function App() {
 				const response = await axios.get(
 					'https://jsonplaceholder.typicode.com/users'
 				);
-				console.log(response);
 				if (response.status !== 200) throw new Error(response.statusText);
 
 				setUsers(
@@ -48,89 +39,12 @@ function App() {
 		return <Loader />;
 	}
 
-	const handleActionIconClick = (action, user) => {
-		switch (action) {
-			case 'like':
-				setUsers((prev) =>
-					prev.map((pu) => {
-						if (pu.id === user.id) {
-							return {
-								...pu,
-								isLiked: !pu.isLiked,
-							};
-						}
-						return pu;
-					})
-				);
-				break;
-			case 'edit':
-				console.log('Edit', user.name);
-				break;
-			case 'delete':
-				setUsers((prev) => prev.filter((pu) => pu.id !== user.id));
-				break;
-			default:
-				break;
-		}
-	};
-
 	return (
 		<div className='App'>
 			<Row gutter={[16, 16]}>
-				{users.map((user) => {
-					const heartIcon = user.isLiked ? (
-						<HeartFilled
-							style={{ color: 'rgb(255, 0, 0)', fontSize: '20px' }}
-							onClick={() => handleActionIconClick('like', user)}
-						/>
-					) : (
-						<HeartOutlined
-							style={{ color: 'rgb(255, 0, 0)', fontSize: '20px' }}
-							onClick={() => handleActionIconClick('like', user)}
-						/>
-					);
-
-					return (
-						<Col key={user.id} xs={24} sm={24} md={8} lg={8} xl={6}>
-							<Card
-								cover={
-									<div className='cardHeaderImage'>
-										<img
-											alt={`Avatar for ${user.username}`}
-											src={`https://api.dicebear.com/9.x/pixel-art/svg?seed=${user.username}`}
-											style={{ height: '200px', width: '200px' }}
-										/>
-									</div>
-								}
-								actions={[
-									heartIcon,
-									<EditOutlined
-										style={{ fontSize: '20px' }}
-										onClick={() => handleActionIconClick('edit', user)}
-									/>,
-									<DeleteFilled
-										style={{ fontSize: '20px' }}
-										onClick={() => handleActionIconClick('delete', user)}
-									/>,
-								]}
-								size='small'
-							>
-								<h3>{user.name}</h3>
-								<p>
-									<MailOutlined /> {user.email}
-								</p>
-								<p>
-									<PhoneOutlined /> {user.phone}
-								</p>
-								<p>
-									<GlobalOutlined />
-									{`http://${user.website}`}
-									{user.website}
-								</p>
-							</Card>
-						</Col>
-					);
-				})}
+				{users.map((user) => (
+					<UserCard user={user} setUsers={setUsers} key={user.id} />
+				))}
 			</Row>
 		</div>
 	);
