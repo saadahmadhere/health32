0f246aa2d75979d5c24e5ebff0e6f942d5fd0fd1
saadahmadhere <saadahmad728@gmail.com>
2025-02-23 11:21:02 +0530

    feat: add configProvider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ConfigProvider } from 'antd';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const theme = {
+	components: {
+		Card: {
+			actionsBg: '#fafafa',
+		},
+	},
+};
 root.render(
 	<React.StrictMode>
-		<App />
+		<ConfigProvider theme={theme}>
+			<App />
+		</ConfigProvider>
 	</React.StrictMode>
 );
